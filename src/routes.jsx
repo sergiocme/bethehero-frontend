771b7d1cadb43ec27994f65b313b2f7b96310817
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-03-26 15:08:27 -0300

    Add router dom to project

---
+++ src/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
+}

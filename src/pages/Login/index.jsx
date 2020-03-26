cc97c0bddb62a550637f1c17e7043f8cdc40d254
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-03-26 15:13:33 -0300

    Use Link component to register on login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 import './style.css';
 
@@ -15,10 +16,10 @@ export default function Login() {
           <input placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
 
-          <a href="/register">
+          <Link to="/register">
             <FiLogIn size={16} color="#e02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />

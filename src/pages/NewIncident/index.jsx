9b22eebb77d41de0faca23bcdc276efc216d6f9b
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-03-28 21:10:17 -0300

    Remove unused import

--- src/pages/NewIncident/index.jsx
+++ src/pages/NewIncident/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Profiler } from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import './style.css';
